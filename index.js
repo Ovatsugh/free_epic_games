f9a36d4ed08551ae9d427561e53d2a50f70ed091
Gustavo Broechl <gustavobroechl25@gmail.com>
2023-04-04 16:18:57 -0300

    Adicionado Dark Theme

--- index.js
+++ index.js
@@ -29,7 +29,31 @@ axios.get('https://api.allorigins.win/get?url=' + encodeURIComponent('https://st
         } else {
             gameList.innerHTML = '⚠️ Sem jogos grátis no momento....';
         }
-
-
-
     })
+
+let temaEscuro = false;
+
+// Verifica se há um estado salvo no armazenamento local
+if (localStorage.getItem('temaEscuro') !== null) {
+  temaEscuro = localStorage.getItem('temaEscuro') === 'true';
+}
+
+// Aplica o tema atual à página
+if (temaEscuro) {
+  document.documentElement.classList.add('dark-mode');
+} else {
+  document.documentElement.classList.remove('dark-mode');
+}
+
+// Adiciona um ouvinte de evento ao ícone do tema para mudar o tema
+const iconeTema = document.querySelector('.fa-moon');
+iconeTema.addEventListener('click', () => {
+  temaEscuro = !temaEscuro;
+  if (temaEscuro) {
+    document.documentElement.classList.add('dark-mode');
+  } else {
+    document.documentElement.classList.remove('dark-mode');
+  }
+  // Salva o estado atual do tema no armazenamento local
+  localStorage.setItem('temaEscuro', temaEscuro);
+});
